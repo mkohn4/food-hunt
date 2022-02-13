3d245733d90b7ea273dc941d7eb252b0e72a71ee
Max Kohn <mkohn@mkohn-ltm4.internal.salesforce.com>
2022-02-13 12:15:31 -0500

    added manifest json to make a PWA

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const webpack  = require('webpack');
+const WebpackPwaManifest  = require('webpack-pwa-manifest');
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
-//require("bootstrap");
 
 
 
@@ -49,6 +49,21 @@ plugins: [
     }),
     new BundleAnalyzerPlugin({
         analyzerMode: "static", //report outputs to an html file in the dist folder
+    }),
+    new WebpackPwaManifest({
+        name: "Food Event",
+        short_name: "Foodies",
+        description: "An app that allows you to view upcoming food events.",
+        start_url: "../index.html",
+        background_color: "#01579b",
+        theme_color: "#ffffff",
+        fingerprints: false,
+        inject: false,
+        icons: [{
+          src: path.resolve("assets/img/icons/icon-512x512.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("assets", "icons")
+        }]
     })
 ],
 mode: 'development'
